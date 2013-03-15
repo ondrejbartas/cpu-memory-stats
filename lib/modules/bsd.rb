4b9621714ebecdcf25e7aa5f1ce883c38241da3a
Ondrej Bartas <ondrej@bartas.cz>
2013-03-15 13:03:01 +0100

    [FIX] Sleep time on FreeBSD system

--- lib/modules/bsd.rb
+++ lib/modules/bsd.rb
@@ -7,25 +7,25 @@ module CpuMemoryStats
   class Bsd
 
     def get_stats
-      
-      output = { :cpu => {} } 
-      
+
+      output = { :cpu => {} }
+
       output[:load_avg] = `sysctl -n vm.loadavg`.gsub(/\{|\}/,"").strip.split(" ").collect{|i| i.strip.to_f.round(2)}
-      
+
       cpu = `sysctl -n kern.cp_time`.strip.split(" ").collect{|i| i.strip.to_i}
       cpu_start = Hash[[:user, :nice, :system, :interupt, :idle].zip(cpu)]
-      sleep(1)
+      sleep(0.01)
       cpu = `sysctl -n kern.cp_time`.strip.split(" ").collect{|i| i.strip.to_i}
       cpu_end = Hash[[:user, :nice, :system, :interupt, :idle].zip(cpu)]
 
       cpu_end.each do |key, value|
         output[:cpu][key] = value - cpu_start[key]
-      end      
+      end
 
       output[:memory] = {
-        :wired => `sysctl -n vm.stats.vm.v_wire_count`.to_i, 
-        :active => `sysctl -n vm.stats.vm.v_active_count`.to_i, 
-        :inactive => `sysctl -n vm.stats.vm.v_inactive_count`.to_i, 
+        :wired => `sysctl -n vm.stats.vm.v_wire_count`.to_i,
+        :active => `sysctl -n vm.stats.vm.v_active_count`.to_i,
+        :inactive => `sysctl -n vm.stats.vm.v_inactive_count`.to_i,
         :used => 0,       #fix missing columns
         :free => `sysctl -n vm.stats.vm.v_free_count`.to_i
       }
